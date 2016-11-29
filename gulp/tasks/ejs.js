c2430a6754cdf144a2b25fe21ffa3c80cc8b59a9
Yuichiro Katsume <y@katsu.me>
2016-11-29 23:34:40 +0900

    Modify

--- gulp/tasks/ejs.js
+++ gulp/tasks/ejs.js
@@ -1,9 +1,11 @@
 var gulp= require('gulp'),
+    plumber= require('gulp-plumber'),
     ejs= require('gulp-ejs'),
 	config= require('../config').ejs;
 
 gulp.task('ejs', function(){
 	return gulp.src(config.src)
+        .pipe(plumber())
         .pipe(ejs(config.options, config.settings))
 		.pipe(gulp.dest(config.dest));
 });
